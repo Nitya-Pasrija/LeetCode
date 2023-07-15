77df7bd1001bc18a816386475f8e496aea448334
Nitya Pasrija <97171261+Nitya-Pasrija@users.noreply.github.com>
2023-07-16 00:19:09 +0530

    Time: 17 ms (28.65%), Space: 43.9 MB (29.07%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,9 +1,21 @@
+import java.util.*;
 public class Solution {
+    public static Map<Character, Integer> makeMap (String a){
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for(int i=0;i<a.length();i++){
+            Character ch = a.charAt(i);
+            if(!mp.containsKey(ch)) mp.put(ch,1);
+            else{
+                int freq = mp.get(ch);
+                mp.put(ch, freq+1);
+            }
+        }
+        return mp;
+    }
     public boolean isAnagram(String s, String t) {
-        int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
-        for (int i = 0; i < t.length(); i++) alphabet[t.charAt(i) - 'a']--;
-        for (int i : alphabet) if (i != 0) return false;
-        return true;
+        if (s.length() != t.length()) return false;
+        Map<Character, Integer> mp1 = makeMap(s);
+        Map<Character, Integer> mp2 = makeMap(t);
+        return mp1.equals(mp2);
     }
 }
